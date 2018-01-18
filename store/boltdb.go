31d8d855a5a221c008b2288b92fa4b1ba7404cf6
Fabio Rinaldi <fabiorino@outlook.com>
2018-01-18 20:05:19 +0100

    BoldtDB implements the store interface

--- store/boltdb.go
+++ store/boltdb.go
@@ -1,1 +1,35 @@
 package store
+
+type BoltDB struct {
+	FilePath string
+}
+
+func (b BoltDB) Init() error {
+
+	return nil
+}
+
+func (b BoltDB) Insert(m ImageMeta) error {
+
+	return nil
+}
+
+func (b BoltDB) Get(guid string) (ImageMeta, error) {
+
+	return ImageMeta{}, nil
+}
+
+func (b BoltDB) Delete(guid string) error {
+
+	return nil
+}
+
+func (b BoltDB) Purge() error {
+
+	return nil
+}
+
+func (b BoltDB) Close() error {
+
+	return nil
+}
